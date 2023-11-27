74974f5276d1d79d870fc8ba4355c433e87577f6
Toure Dunnon <tdunnon@gmail.com>
2023-11-27 16:29:46 -0500

    Initial commit

---
+++ test/make_node_group.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as MakeNodeGroup from '../lib/make_node_group-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/make_node_group-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new MakeNodeGroup.MakeNodeGroupStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
